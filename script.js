f3e489f68ca537ce2f46dd70c8801d716c1b5444
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-01-11 07:56:07 -0300

    challenge 08 done

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
-var botao = window.document.getElementById("submit");
-botao.addEventListener("mousedown", submit);
-botao.addEventListener("mouseup", botaoSai);
+let nota = 0
+let main = document.getElementsByClassName('main')[0];
+let estrela = document.getElementsByClassName('estrela')[0];
+let titulo = document.getElementsByTagName('h1')[0];
+let thanksImg = document.getElementsByClassName('thanks-img')[0]
+let paragrafo = document.getElementsByTagName('p')[0]
+let span = document.getElementsByTagName('span')[0];
+let botao = window.document.getElementById("submit");
+let notaMsg = document.getElementsByClassName("ratingMsg")[0];
 
-function gethate(valor){
-    var nota = valor.value
-    
+botao.addEventListener("mouseup", submit);
+botao.addEventListener("mousedown", botaoDown);
+
+function getRate(valor){
+    nota = valor.value
+
 };
 
 function submit(){
     botao.style.backgroundColor = 'white';
     botao.style.color = '#fb7413';
+    estrela.style.display = 'none';
+    span.style.display = 'none';
+    botao.style.display = 'none';
+    titulo.innerHTML = 'Thank you!'
+    paragrafo.innerHTML = 'We appreciate you taking the time to give a rating.  If you ever need more support, don’t hesitate to get in touch!'
+    thanksImg.style.display = "block";
+    notaMsg.style.display='block';
+    notaMsg.innerHTML = `You selected ${nota} out of 5`;
+    thanksImg.style.margin = "auto";
+    main.classList.add('thanks_page');
 };
 
-function cleamHatePage(){
-    let estrelas = document.getElementsByClassName("staricon")
-    for (let i = 0; i<=estrelas.length;i++){
-        estrelas[i].style.visibility = 'hidden';
-    }
-    
-}
-function funcao(){
-    let img_cont = document.getElementById('img-countainer')
-    let imgt = document.createElement("img")
-    imgt.src = "imagens/illustration-thank-you.svg";
-    img_cont.appendChild(imgt);
-}
-function botaoSai(){
-    botao.style.backgroundColor = '#fb7413';
-    botao.style.color = 'white';
+function botaoDown(){
+    botao.style.backgroundColor = 'white';
+    botao.style.color = '#fb7413';
 };
 
 function mouseOnCircle(opcao){
-    opcao.style.backgroundColor = "#7c8798";
+        opcao.style.backgroundColor = "#7c8798"; 
 
 }
 function mouseOutCircle(opcao){
-    opcao.style.backgroundColor = "#262F38";
+    opcao.style.backgroundColor = "#262F38"; //volta a cor normal
 }
 
 function mouseDownCircle(opcao){
